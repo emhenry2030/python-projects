c5c2908510ba08aaa0014c8380658ee8c80ac312
Emily Henry <emhenry2030@icloud.com>
2022-12-14 16:51:16 -0500

    making it better

--- shelby.py
+++ shelby.py
@@ -1,3 +1,5 @@
-print('howdy ya'll)
-print('What is your name?')/>
+print("Howdy, this is the quizmaster. I will be asking you a few questions.")
+print("The first question is: What is your name")
 name = input()
+print("Hi," + name )
+ 
