1fadf01c358aad721155b6fb45fec249b725baea
Emily Henry <emhenry2030@icloud.com>
2023-11-25 07:39:09 -0500

    added second question

--- shelby.py
+++ shelby.py
@@ -1,5 +1,10 @@
-print("Howdy, this is the quizmaster. I will be asking you a few questions.")
+print("Howdy, this is the quizmaster. I will be asking you a few questions.Please DO NOT use capital letters! ")
 print("The first question is: What is your name")
 name = input()
-print("Hi," + name )
- 
+print("hi," + name)
+
+print("The second question is: What is your favorite movie")
+movie = input()
+print("My favorite movie is Home Alone, but" + movie + "is a good one too.")
+
+map
